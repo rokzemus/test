89a461d061f5265ad7f189b8631784ecf97814a4
Jacob <jgharris314@gmail.com>
2020-03-15 06:03:26 -0500

    added Person class to main file

---
+++ Person.py
@@ -0,0 +1,13 @@
+''' Class Person: an individual to be scheduled on the road map. @name: the Persons name. @startTime: the time the
+    individual is scheduled to work. @outTime: the time the individual is meant to leave work. @gamesKnow: a list of
+    table games that the individual is capable of dealing.
+'''
+
+
+class Person:
+    def __init__(self, name, startTime, outTime, gamesKnown):
+        self.name = name
+        self.startTime = startTime
+        self.outTime = outTime
+        self.gamesKnown = gamesKnown
+
