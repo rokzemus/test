36639690e9b2703753cbfa00481d6c8c9d7b2df8
rokzemus <james.hanna87@gmail.com>
2020-03-19 17:34:17 -0600

    new file

--- importing.py
+++ importing.py
@@ -7,5 +7,5 @@ employee_List = []
 with open('schedule.csv', 'r') as Schedule:
     reader = csv.reader(Schedule)
     for row in reader:
-        employee_List.append(Person(row[0],row[1],row[2]))
+        employee_List.append(Person.Person(row[0],row[1],row[2]))
 print(employee_List)
