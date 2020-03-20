7d1cb4feb354776e382528da265910f911f0c87a
Jacob <jgharris314@gmail.com>
2020-03-19 19:11:35 -0500

    test

--- importing.py
+++ importing.py
@@ -12,3 +12,11 @@ with open('schedule.csv', 'r') as Schedule:
         employee_List.append(Person.Person(row[0],row[1],row[2]))
 for i in range(len(employee_List)):
     print(employee_List[i])
+
+blackJackOne = Table.Table("BJ", True, 1, 1, employee_List[1].name)
+
+print(blackJackOne.dealerName)
+
+blackJackOne.dealerName = employee_List[0].name
+
+print(blackJackOne.dealerName)
